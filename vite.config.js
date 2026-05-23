9144c5253d82e1e0b5c078d7a87bd8a398f1cd47
shubhpatil28 <shubhampatil282487@gmail.com>
2026-05-23 19:18:12 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:5000',
+          changeOrigin: true,
+          secure: false,
+        },
+        '/socket.io': {
+          target: 'http://127.0.0.1:5000',
+          changeOrigin: true,
+          ws: true,
+          secure: false,
+        },
+      },
+  },
+})
+

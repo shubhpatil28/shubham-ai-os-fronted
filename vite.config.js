ebb4682be6bf0f0632029f4b9aead509af914c66
shubhpatil28 <shubhampatil282487@gmail.com>
2026-05-24 22:48:21 +0530

    Refactor server proxy configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,21 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
- server: {
-  proxy: {
-    '/api': {
-      target: 'https://shubham-ai-backend.onrender.com',
-      changeOrigin: true,
-      secure: true,
-    },
 
-    '/socket.io': {
-      target: 'https://shubham-ai-backend.onrender.com',
-      changeOrigin: true,
-      ws: true,
-      secure: true,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://shubham-ai-backend.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      },
+
+      '/socket.io': {
+        target: 'https://shubham-ai-backend.onrender.com',
+        changeOrigin: true,
+        ws: true,
+        secure: true,
+      },
     },
   },
-},
-
+})

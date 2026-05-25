3fdbbb5c797541d8148b1262d5f41dfe0a84249c
shubhpatil28 <shubhampatil282487@gmail.com>
2026-05-25 18:59:35 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,30 @@
+import { useEffect } from "react";
+
+const API_URL = "https://shubham-ai-backend.onrender.com";
+
 export default function App() {
+
+  useEffect(() => {
+    fetch(`${API_URL}/`)
+      .then((res) => res.text())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div
       style={{
-        background: '#050816',
-        color: 'cyan',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: '40px',
-        fontWeight: 'bold',
+        background: "#050816",
+        color: "cyan",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px",
+        fontWeight: "bold",
       }}
     >
       SHUBHAM AI OS 🚀
     </div>
-  )
+  );
 }
